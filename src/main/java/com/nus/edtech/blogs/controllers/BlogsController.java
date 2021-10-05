e4c1c67fb5c36e394beba56ba0158271cc5a3d9f
Niya Aniyan <niya.aniyan@u.nus.edu>
2021-10-05 21:09:39 +0530

    modified updateInteraction return type

--- src/main/java/com/nus/edtech/blogs/controllers/BlogsController.java
+++ src/main/java/com/nus/edtech/blogs/controllers/BlogsController.java
@@ -7,10 +7,7 @@ import com.nus.edtech.blogs.models.Interaction;
 import com.nus.edtech.blogs.services.BlogsService;
 import ma.glasnost.orika.MapperFacade;
 import org.springframework.beans.factory.annotation.Autowired;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-//import com.edtech.qaservice.model.*;
 import java.util.List;
 import java.util.UUID;
 
@@ -19,33 +16,11 @@ import java.util.UUID;
 
 public class BlogsController {
 
-    private static final String QNA_SERVICE = "qnaService";
-
     @Autowired
     private BlogsService blogsService;
 
     @Autowired
     private MapperFacade mapperFacade;
-
-    @Autowired
-    private RestTemplate restTemplate;
-
-    @GetMapping("questions/all")
-    @CircuitBreaker(name=QNA_SERVICE, fallbackMethod= "qnaFallback")
-    public String getMyOrder(){
-        try {
-            return restTemplate.getForObject
-                    ("http://localhost:9003/v1/order",
-                            String.class);
-
-        } catch (Exception ex) {
-            throw ex;
-        }
-    }
-
-    public String qnaFallback(Exception e){
-        return "test error";
-    }
 
     @GetMapping("all")
     public List<Blogs> getAllBlogs(){
@@ -57,6 +32,7 @@ public class BlogsController {
         }
 
     }
+
     @PostMapping("author/{author}")
     public boolean postBlogByAuthor(@PathVariable(value = "author") String author, @RequestBody Blogs requestBlog) throws Exception {
         if (requestBlog == null || author == null)
@@ -154,13 +130,13 @@ public class BlogsController {
     }
 
     @PutMapping("blog/{blogid}/interaction")
-    public boolean updateInteractionToBlog(@PathVariable(value = "blogid") String blogId,
+    public String updateInteractionToBlog(@PathVariable(value = "blogid") String blogId,
                                            @RequestBody Interaction interaction) throws Exception {
         try {
             if(blogId == null || interaction == null)
                 throw new Exception("Input blogId or interaction is null for updateInteractionToBlog");
             blogsService.updateInteractionToBlog(blogId,interaction);
-            return true;
+            return "User interaction is added to the blog";
         }catch (Exception ex){
             throw ex;
         }
