fe3060890c129f02ce9926d92dabfdc6867b84cd
Jiang Yue <290836756@qq.com>
2021-10-12 13:50:53 +0800

    add message queue listener

--- src/main/java/com/nus/edtech/blogs/BlogsApplication.java
+++ src/main/java/com/nus/edtech/blogs/BlogsApplication.java
@@ -1,15 +1,21 @@
 package com.nus.edtech.blogs;
 
+import com.nus.edtech.blogs.listener.MessageQueueListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import java.beans.BeanProperty;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class BlogsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogsApplication.class, args);
+		Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(new MessageQueueListener(), 10, 1, TimeUnit.SECONDS);
 	}
 
 	@Bean
