b811d8697cb135c4a3980400d25ed67580e787f7
Niya Aniyan <niya.aniyan@u.nus.edu>
2021-10-13 00:04:02 +0530

    swagger integration

--- src/main/java/com/nus/edtech/blogs/BlogsApplication.java
+++ src/main/java/com/nus/edtech/blogs/BlogsApplication.java
@@ -5,12 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
-import java.beans.BeanProperty;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class BlogsApplication {
 
 	public static void main(String[] args) {
