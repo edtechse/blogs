c08291761c0f432fe743f68a4a3eea859cb956c3
Niya Aniyan <niya.aniyan@u.nus.edu>
2021-10-12 16:26:16 +0530

    Added JUnit tests

--- src/test/java/com/nus/edtech/blogs/controllers/BlogsControllerTests.java
+++ src/test/java/com/nus/edtech/blogs/controllers/BlogsControllerTests.java
@@ -36,12 +36,15 @@ public class BlogsControllerTests {
 
     private List<BlogsEntity> blogsList;
 
+    private List<String> blogIds;
+
     public BlogsControllerTests() throws Exception {
     }
 
     @Before
     public void init(){
         blogsList = new ArrayList<BlogsEntity>();
+        blogIds = new ArrayList<String>();
         blogsEntity = new BlogsEntity();
         blogsEntity.setBlogId("1234");
         blogsEntity.setBlogAuthor("u1");
@@ -55,6 +58,7 @@ public class BlogsControllerTests {
         blogsEntity.setBlogTags(tags);
         blogsEntity.setBlogCreationDate("11-11-2021");
         blogsList.add(blogsEntity);
+        blogIds.add("1234");
     }
 
     @Test
@@ -63,7 +67,41 @@ public class BlogsControllerTests {
         List<Blogs> blogsResponse = blogsController.getAllBlogs();
 
         Assert.assertEquals(blogsList.size(),blogsResponse.size());
+        Assert.assertEquals(blogsList.get(0).getBlogId(),blogsResponse.get(0).getBlogId());
+        Assert.assertEquals(blogsList.get(0).getBlogAuthor(),blogsResponse.get(0).getBlogAuthor());
+        Assert.assertEquals(blogsList.get(0).getBlogTitle(),blogsResponse.get(0).getBlogTitle());
+        Assert.assertEquals(blogsList.get(0).getBlogText(),blogsResponse.get(0).getBlogText());
+        Assert.assertEquals(blogsList.get(0).getBlogTags(),blogsResponse.get(0).getBlogTags());
+        Assert.assertEquals(blogsList.get(0).getBlogCreationDate(),blogsResponse.get(0).getBlogCreationDate());
+    }
 
+    @Test
+    public void getAllBlogs_isEmpty() {
+        List<BlogsEntity> emptyBlogsEntity = new ArrayList<BlogsEntity>();
+        Mockito.when(blogsService.getAllBlogs()).thenReturn(emptyBlogsEntity);
+        List<Blogs> blogItemsActual = blogsController.getAllBlogs();
+        Assert.assertEquals(new ArrayList() {}, blogItemsActual);
+        Assert.assertEquals(0,blogItemsActual.size());
+    }
 
+    @Test
+    public void getBlogIdsByAuthor_isSuccess() throws Exception {
+
+        Mockito.when(blogsService.findBlogsByAuthor("u1")).thenReturn(blogIds);
+        List<String> blogIdsActual = blogsController.getBlogIdsByAuthor("u1");
+
+        Assert.assertEquals(blogIds.size(),blogIdsActual.size());
+        Assert.assertEquals(blogIds.get(0),blogIdsActual.get(0));
+    }
+
+    @Test
+    public void getBlogIdsByAuthor_isEmpty() throws Exception {
+
+        List<String> emptyList = new ArrayList<String>();
+        Mockito.when(blogsService.findBlogsByAuthor("u1_nonexist")).thenReturn(emptyList);
+        List<String> blogIdsActual = blogsController.getBlogIdsByAuthor("u1_nonexist");
+
+        Assert.assertEquals(new ArrayList(){},blogIdsActual);
+        Assert.assertEquals(0,blogIdsActual.size());
     }
 }
